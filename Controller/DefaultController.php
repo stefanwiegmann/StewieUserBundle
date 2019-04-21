15e922f61bd08252f277b26679d8063d1b62e359
Stefan Wiegmann <mail@stefanwiegmann.de>
2019-04-21 16:14:50 -0400

    added default controlle

---
+++ Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Stefanwiegmann\UserBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+class DefaultController extends AbstractController
+{
+    /**
+    * @Route("/user", name="user_default")
+    */
+    public function default()
+    {
+      return $this->render('@stefanwiegmann_user/default/index.html.twig', [
+          'headline' => 'User Default',
+          'content' => 'hello user',
+      ]);
+    }
+}
