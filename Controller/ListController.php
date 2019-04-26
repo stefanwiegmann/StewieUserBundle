ddca647179af857cf2246dc25e4040251cf6c63b
Stefan Wiegmann <mail@stefanwiegmann.de>
2019-04-25 22:31:58 -0400

    first user list

--- Controller/ListController.php
+++ Controller/ListController.php
@@ -2,21 +2,47 @@
 
 namespace App\Stefanwiegmann\UserBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+// use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
-class ListController extends AbstractController
+class ListController extends Controller
 {
     /**
-    * @Route("/user/list", name="sw_user_list")
+    * @Route("/user/list/{page}", defaults={"page": 1}
+    *     , requirements={"page": "\d+"}, name="sw_user_list")
     */
-    public function list()
+    public function list($page, Request $request)
     {
+      //get data and paginate
+      $paginator  = $this->get('knp_paginator');
+      $pagination = $paginator->paginate(
+      $this->getQuery(), /* query NOT result */
+      $request->query->getInt('page', $page)/*page number*/,
+            $this->container->getParameter('max_rows')/*limit per page*/
+        );
+        // $pagination->setTemplate('@SWUser/User/pagination.html.twig');
+        // $pagination->setTemplate('@SWUser/User/pagination.html.twig');
+
       return $this->render('@stefanwiegmann_user/list/list.html.twig', [
-          'headline' => 'User Default',
-          'content' => 'hello user',
+          'userList' => $pagination,
+          'page' => $page,
       ]);
+    }
+
+    public function getQuery(){
+
+        $repository = $this->getDoctrine()
+          ->getRepository('StefanwiegmannUserBundle:User');
+
+        $query = $repository->createQueryBuilder('u')
+          ->orderBy('u.id', 'ASC');
+
+          return $query
+            ->getQuery();
+
     }
 }
